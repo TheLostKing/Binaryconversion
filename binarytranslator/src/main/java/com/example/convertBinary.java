dd402affe72bf9a8a2e17c14771b082389300fa2
TheLostKing <calderonezj@gmail.com>
2015-09-15 13:44:26 -0500

    Initial commit

---
+++ binarytranslator/src/main/java/com/example/convertBinary.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class convertBinary {
+    public static void main(String[] args)
+    {
+        int binary = 101001;
+        int converted = 0;
+
+        System.out.println(101001/100000);
+        System.out.println(101001/10000);
+    }
+}
